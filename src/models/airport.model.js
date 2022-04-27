526b4fe112af56534ebdcc52079fe090b2924579
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-04-27 23:06:05 +0530

    airport added

--- src/models/airport.model.js
+++ src/models/airport.model.js
@@ -2,7 +2,16 @@ const mongoose = require("mongoose");
 
 const airportSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    city: { type: String, required: true }
+    city: { type: String, required: true },
+    start: { type: String, required: true },
+    start_id: { type: String, required: true },
+    end: { type: String, required: true },
+    end_id: { type: String, required: true },
+    cost: { type: String, required: true },
+    start_time: { type: String, required: true },
+    end_time: { type: String, required: true },
+    PNR: { type: String },
+    capacity: { type: Number }
 },
 
     {
